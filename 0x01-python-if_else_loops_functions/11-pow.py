0c25c54b5094e59d4c013ee91edfc0efafda4345
MaazAlside <maazalside@gmail.com>
2023-03-09 02:55:05 +0300

    hot fix

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def pow(a, b):
+    if b == 0:
+        return 1
+    elif b < 0:
+        return 1 / pow(a, -b)
+    else:
+        result = a
+        for i in range(1, b):
+            result *= a
+        return result
