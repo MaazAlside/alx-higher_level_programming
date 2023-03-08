dcf3080b91b408b6551766954690190226a887c1
MaazAlside <maazalside@gmail.com>
2023-03-09 01:54:35 +0300

    hot fix

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-for i in range(10):
+for i in range(1, 10):
     for j in range(i+1, 10):
-        print("{0:02d}, {1:02d}".format(i, j), end=", "
-              if i < 8 or j < 9 else "\n")
+        if i != j:
+            print("{:d}{:d}".format(i, j), end="")
+            if i != 8 or j != 9:
+                print(", ", end="")
+            else:
+                print()
