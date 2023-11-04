94a01401f5b097d012260f039670b18998613fe9
maazalside <maazalside@gmail.com>
2023-11-04 11:16:24 +0300

    Task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = [letter for letter in my_string if letter not in "Cs"]
-    return "".join(new_string)
+    if my_string:
+        new_string = [letter for letter in my_string if letter not in "Cs"]
+        return "".join(new_string)
