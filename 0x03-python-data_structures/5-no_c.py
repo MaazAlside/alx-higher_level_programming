e3b4e695d02c59e6158ae5c2c89c521acc3ace1d
maazalside <maazalside@gmail.com>
2023-11-04 11:24:13 +0300

    Task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = [letter for letter in my_string if letter != "C" and letter != "c"]
+    new_string = [letter for letter in my_string
+                  if letter != "C" and letter != "c"]
     return "".join(new_string)
