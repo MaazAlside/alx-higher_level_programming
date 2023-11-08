97247a15ce608b5dcab7e1bacb141134f02fcabc
maazalside <maazalside@gmail.com>
2023-11-08 20:02:30 +0300

    Task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-        
+
     if sys.argv[2] not in ['+', '-', '*', '/']:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
@@ -14,13 +14,13 @@ if __name__ == "__main__":
     num1 = int(sys.argv[1])
     op = (sys.argv[2])
     num2 = int(sys.argv[3])
-    
+
     if op == '+':
         print("{} + {} = {}".format(num1, num2, add(num1, num2)))
     elif op == '-':
-            print("{} - {} = {}".format(num1, num2, sub(num1, num2)))
+        print("{} - {} = {}".format(num1, num2, sub(num1, num2)))
     elif op == '*':
-            print("{} * {} = {}".format(num1, num2, mul(num1, num2)))
+        print("{} * {} = {}".format(num1, num2, mul(num1, num2)))
     elif op == '/':
         if num2 == 0:
             print("Error: Division by zero")
