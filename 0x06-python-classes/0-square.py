f475d13c5078aa24c1a8e0fc2ff95bb05fbe34f8
maazalside <maazalside@gmail.com>
2023-11-21 18:48:22 +0300

    Task 0

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+class Square:
+    """
+    A Class Square
+    Attributes:
+    Nothing
+"""
+    def __init__(self):
+        """
+        Initialize a new Square
+        Args:
+        Nothing
+        """
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+class Square:
+	"""
+	A Class Square
+
+	Attributes:
+		size : private square size
+
+	def __init__(self, size):
+		"""
+		Initilaze a new Square
+
+		Args:
+			size : the size of Square
+		"""
+		self.__size = size
