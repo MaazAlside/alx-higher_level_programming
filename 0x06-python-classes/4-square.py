fc116f2898ecdeef18b164fca4439a3e3480011b
maazalside <maazalside@gmail.com>
2023-11-21 20:53:22 +0300

    4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+"""class Square that defines a square"""
+
+
+class Square:
+    """class Square that defines a square
+
+    Attributes:
+    __size : private square size
+    """
+    def __init__(self, size=0):
+        """Initialize a new Square
+
+        Args:
+        size (int): the size of Square
+        """
+        try:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+        except TypeError:
+            raise TypeError("size must be an integer")
+        self.__size = int(size)
+
+    @property
+    def size(self):
+        """Return the size of the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size with value"""
+        try:
+            if value < 0:
+                raise ValueError("size must be >= 0")
+        except TypeError:
+            raise TypeError("size must be an integer")
+        self.__size = int(value)
+
+    def area(self):
+        """Return the area of the square"""
+        return self.__size ** 2
