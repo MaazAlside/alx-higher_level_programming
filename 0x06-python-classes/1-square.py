034fb46972218ef87e26f5b3a1d6abd618c3a4c7
maazalside <maazalside@gmail.com>
2023-11-21 19:50:36 +0300

    fix style

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,15 +4,16 @@
 
 
 class Square:
-	"""A class representing a square.
- 
-	Attributes:
-		__size : private square size
-	"""
-	def __init__(self, size):
-		"""Initialize a new Square
-     
-     	Args:
-     		size (int): the size of Square
-   		"""
-		self.__size = size
+    """A class representing a square.
+
+    Attributes:
+    __size : private square size
+    """
+
+    def __init__(self, size):
+        """Initialize a new Square
+
+        Args:
+        size (int): the size of Square
+        """
+        self.__size = size
