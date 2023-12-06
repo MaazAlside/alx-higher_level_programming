25afc2a59d628974e6cd8c7c4082aefc8b7f5246
maazalside <maazalside@gmail.com>
2023-12-06 21:57:07 +0300

    1-my_list.py added

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,1 +1,41 @@
 #!/usr/bin/python3
+
+class MyList(list):
+    """
+    MyList is a custom class that extends the built-in list class.
+
+    This class provides additional functionality for working with lists.
+
+    Attributes:
+        No additional attributes are defined.
+
+    Methods:
+        __init__: Initializes an instance of the MyList class.
+        print_sorted: Prints the elements of the list in sorted order.
+    """
+
+    def __init__(self):
+        """
+        Initialize an instance of the MyList class.
+
+        Parameters:
+            *args: Variable-length argument list,
+            allowing the initialization of the list with values.
+
+        Returns:
+            None
+        """
+        super(MyList, self).__init__()
+
+    def print_sorted(self):
+        """
+        Print the elements of the list in sorted order.
+
+        Parameters:
+            None
+
+        Returns:
+            None
+        """
+        sortedlist = sorted(self)
+        print(sortedlist)
