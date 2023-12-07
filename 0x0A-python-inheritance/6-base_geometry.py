fec80b145ca8db81300ecadead9a9f5a8c9c59e7
maazalside <maazalside@gmail.com>
2023-12-07 14:17:47 +0300

    6-base_geometry.py

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+
+"""an empty class """
+
+
+class BaseGeometry:
+    """Empty Class """
+
+    def __init__(self):
+        """inilize the opject"""
+        pass
+
+    def area(self):
+        """area not emplemented"""
+        raise Exception("area() is not implemented")
