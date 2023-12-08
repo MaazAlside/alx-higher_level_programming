36e845c7ffb2b987daabdc3b648716dadabb57c4
maazalside <maazalside@gmail.com>
2023-12-08 20:45:16 +0300

    fix error

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -4,13 +4,15 @@
 
 
 def append_write(filename="", text=""):
-    """append a string to a text file (UTF-8).
+    """
+    Append a string to a text file (UTF-8).
 
     args:
     filename: The name of the file to be written.
     text: The string to be written to the file.
 
-    return: The number of characters added.
+    Return:
+    The number of characters added.
     """
 
     with open(filename, mode="a", encoding="utf-8") as file:
