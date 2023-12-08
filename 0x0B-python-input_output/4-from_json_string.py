4911c32af16aa4e2d571b6dd546c2899c359e793
maazalside <maazalside@gmail.com>
2023-12-08 22:24:47 +0300

    4-from_json_string.py

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+
+"""returns the object representation of JSON"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    Convert a JSON representation to  Python object.
+
+    args
+    obj: The Python object to be converted.
+
+    Return
+    The JSON-formatted string.
+    """
+    return json.loads(my_str)
