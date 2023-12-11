0b520aee89ccaa94587e5bb2c8ebd6f8f9b42857
maazalside <maazalside@gmail.com>
2023-12-11 21:14:23 +0300

    base.py added

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+
+"""0. If it's not tested it doesn't work"""
+
+
+class Base:
+    """Base class for other classes in the module."""
+
+    def __init__(self, id=None):
+        """Initialize a Base instance.
+
+        Args:
+            id (int): An optional ID for the instance. If not provided,
+                      a default ID will be assigned.
+        """
+        __nb_objects = 0
+        if id is not None:
+            self.id = id
+        else:
+            __nb_objects += 1
+            self.id = __nb_objects
