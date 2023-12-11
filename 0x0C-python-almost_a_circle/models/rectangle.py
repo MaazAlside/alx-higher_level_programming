9845f4dd18aa1f1b7e6317181828ffe45c8a5780
maazalside <maazalside@gmail.com>
2023-12-11 22:53:45 +0300

    add class Rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+
+"""A class Rectangle that inherits from Base."""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """A class representing a rectangle, inheriting from the Base class."""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize a Rectangle instance.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+            x (int): The x-coordinate of the rectangle's position.
+            y (int): The y-coordinate of the rectangle's position.
+            id (int): An optional ID for the instance.
+        """
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+        super().__init__(id)
