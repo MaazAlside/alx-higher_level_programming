315ff1fb0f6f29afedce6791658cb812d9e1e1ee
maazalside <maazalside@gmail.com>
2023-12-12 11:35:40 +0300

    update disply fun

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -100,7 +100,11 @@ class Rectangle(Base):
 
     def display(self):
         """prints in stdout the Rectangle instance with the character #"""
+        for row in range(self.__y):
+            print()
         for row in range(self.__height):
+            for col in range(self.__x):
+                print(" ", end="")
             for col in range(self.__width):
                 print("#", end="")
             print()
