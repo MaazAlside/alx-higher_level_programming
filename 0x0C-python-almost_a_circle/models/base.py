2cd7653897951183c797d07eb038389353bb14a5
maazalside <maazalside@gmail.com>
2023-12-13 23:04:18 +0300

    Task 14, 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 """0. If it's not tested it doesn't work"""
+import json
 
 
 class Base:
@@ -20,3 +21,16 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Convert a list of dictionaries to a JSON-formatted string."""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Save a list of instances to a JSON file."""
+        with open("Rectangle.json", 'w') as file:
+            json.dump([obj.to_dictionary() for obj in list_objs], file)
