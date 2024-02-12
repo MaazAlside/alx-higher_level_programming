15d310cd57af0d6a7227e6427ed11e09f31ae8ae
maazalside <maazalside@gmail.com>
2024-02-13 01:23:42 +0300

    fix 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,23 +1,8 @@
 #!/usr/bin/node
-
-if (!process.argv[2] || parseInt(process.argv[2]) === 1) {
+const args = process.argv.slice(2).map(Number);
+if (args.length <= 1) {
   console.log(0);
 } else {
-  let firstBiggest = parseInt(process.argv[2]);
-  let secondBiggest = 0;
-
-  for (let i = 2; i < process.argv.length; i++) {
-    const currentNumber = parseInt(process.argv[i]);
-
-    if (!isNaN(currentNumber)) {
-      if (currentNumber > firstBiggest) {
-        secondBiggest = firstBiggest;
-        firstBiggest = currentNumber;
-      } else if (currentNumber > secondBiggest) {
-        secondBiggest = currentNumber;
-      }
-    }
-  }
-
-  console.log(secondBiggest);
+  const sortedArgs = args.sort((a, b) => b - a);
+  console.log(sortedArgs[1]);
 }
