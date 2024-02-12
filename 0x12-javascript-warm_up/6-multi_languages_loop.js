87ea85c10f860d3149fae11be03bf30f3b2d6bc1
maazalside <maazalside@gmail.com>
2024-02-13 01:25:21 +0300

    fix small bug in task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const array = ['C is fun', 'Python is coo', 'JavaScript is amazing'];
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (const statement of array) {
   console.log(statement);
 }
