556e94ff277df0f3ea99f3aca8b58f11e1762dd8
maazalside <maazalside@gmail.com>
2024-02-13 01:20:54 +0300

    fix bug in 6 & 11 tasks git push

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,16 +1,23 @@
 #!/usr/bin/node
-if (!process.argv[2] || parseInt(process.argv[2] === 1)) {
+
+if (!process.argv[2] || parseInt(process.argv[2]) === 1) {
   console.log(0);
 } else {
   let firstBiggest = parseInt(process.argv[2]);
   let secondBiggest = 0;
+
   for (let i = 2; i < process.argv.length; i++) {
-    if (parseInt(process.argv[i]) > firstBiggest) {
-      secondBiggest = firstBiggest;
-      firstBiggest = parseInt(process.argv[i]);
-    } else if (parseInt(process.argv[i]) > secondBiggest) {
-      secondBiggest = parseInt(process.argv[i]);
+    const currentNumber = parseInt(process.argv[i]);
+
+    if (!isNaN(currentNumber)) {
+      if (currentNumber > firstBiggest) {
+        secondBiggest = firstBiggest;
+        firstBiggest = currentNumber;
+      } else if (currentNumber > secondBiggest) {
+        secondBiggest = currentNumber;
+      }
     }
   }
+
   console.log(secondBiggest);
 }
