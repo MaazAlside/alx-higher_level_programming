556ed50a4ba9a843e6421c33d14cf604dfbea167
maazalside <maazalside@gmail.com>
2024-02-14 09:00:56 +0300

    Task 100

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const list = require('./100-data').list;
+const { list } = require('./100-data');
 const newArray = list.map((x, i) => x * i);
 console.log(list);
 console.log(newArray);
