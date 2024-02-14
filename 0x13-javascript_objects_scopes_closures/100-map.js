9744eb12f067f2a67b20d5eefa69aa4217c89e29
maazalside <maazalside@gmail.com>
2024-02-14 08:58:27 +0300

    fix bug

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
-
+const newArray = list.map((x, i) => x * i);
 console.log(list);
-console.log(list.map((x, i) => x * i));
+console.log(newArray);
