169e2676ff5e464089ecab9898e7709c56dc2973
maazalside <maazalside@gmail.com>
2024-02-18 00:25:22 +0300

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""Script that lists all states from the database hbtn_0e_0_usa"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM `states` WHERE `name` LIKE 'N%'")
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
