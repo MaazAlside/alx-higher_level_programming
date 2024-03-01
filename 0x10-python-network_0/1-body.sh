d11d880dac0164efdc886fc7137cf76042c26735
maazalside <maazalside@gmail.com>
2024-03-01 19:13:29 +0300

    1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes in a URL, sends a GET request to the
-curl -sI "$1" | grep -i 'HTTP/1.1 200' | curl -s "$1"
+curl -sI "$1" | grep -i 'HTTP/1.1 200' > /dev/null && curl -s "$1"
