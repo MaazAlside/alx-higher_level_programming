ecb643462a36c73dae7cf42416850925b7b22e73
maazalside <maazalside@gmail.com>
2024-03-02 10:54:34 +0300

    6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+""" Finds a peak in a list of unsorted integers. """
+
+
 def find_peak(list_of_integers):
-    if list_of_integers:
-        return max(list_of_integers)
-    else:
+    """Finds a peak in a list of unsorted integers."""
+    if not list_of_integers:
         return None
+
+    n = len(list_of_integers)
+
+    for i in range(n):
+        if list_of_integers[i] >= list_of_integers[i + 1] and \
+           list_of_integers[i] >= list_of_integers[i - 1]:
+            return list_of_integers[i]
