e5d45b297ba6f6490cb187bc021ae3419837b176
maazalside <maazalside@gmail.com>
2024-03-03 22:31:14 +0300

    2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+""" POST Script to send email """
+import sys
+import urllib.request
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    url_value = sys.argv[2]
+    
+    req = urllib.request.Request(url, data=url_value.encode('utf-8'))
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode())
