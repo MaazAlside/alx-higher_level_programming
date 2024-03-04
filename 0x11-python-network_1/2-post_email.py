1d4f45fdfea83e4b5ce7655bafbd6b037f2cc16c
maazalside <maazalside@gmail.com>
2024-03-04 19:45:05 +0300

    2-post_email.py fix error

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,11 +2,14 @@
 """ POST Script to send email """
 import sys
 import urllib.request
+import urllib.parse
 
 if __name__ == '__main__':
     url = sys.argv[1]
-    url_value = sys.argv[2]
+    email_value = sys.argv[2]
 
-    req = urllib.request.Request(url, data=url_value.encode('utf-8'))
+    data = urllib.parse.urlencode({'email': email}).encode('utf')
+
+    req = urllib.request.Request(url, data=data)
     with urllib.request.urlopen(req) as response:
         print("Your email is: " + response.read().decode())
