ceb82071a4c8cd952fb244dcf704056a3bb956de
maazalside <maazalside@gmail.com>
2024-03-05 23:27:56 +0300

    3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-""" """
+"""  Python script that takes in a URL, sends a request to the URL """
 import urllib.request
 import sys
 import urllib.error
 
 if __name__ == "__main__":
-    req = urllib.request.Request(sys.argv[1])
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
     try:
-        with urllib.request.urlopen(sys.argv[1]) as response:
+        with urllib.request.urlopen(url) as response:
             print(response.read().decode('ascii'))
     except urllib.error.HTTPError as e:
-            print("Error code: " + e.code)
+            print("Error code: " + str(e.code))
