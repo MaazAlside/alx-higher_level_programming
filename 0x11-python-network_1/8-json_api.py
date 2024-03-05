46f5da2e8b9deb0f6d6ec93eded0c3c4bb01309e
maazalside <maazalside@gmail.com>
2024-03-06 00:27:13 +0300

    8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+""" script that takes in a letter
+    and sends a POST request to http://0.0.0.0:5000/search_user
+    with the letter as a parameter.
+    """
+import requests
+import sys
+
+if __name__ == "__main__":
+    letter = {'q': sys.argv[1]}
+
+    req = requests.post('http://0.0.0.0:5000/search_user', data=letter)
+    try:
+        response = req.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get('id'), response.get('name')))
+    except ValueError:
+        print("Not a valid JSON")
