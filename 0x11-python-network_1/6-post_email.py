86ac529977756d8ca07e74b4fe792486218b911f
maazalside <maazalside@gmail.com>
2024-03-05 23:52:50 +0300

    6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+""" """
+
+import sys
+import requests
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    email = sys.argv[2]
+    
+    res = requests.post(url, data={'email': email})
+    print("Your email is: {}".format(res.text))
