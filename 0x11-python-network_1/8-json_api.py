e6cf22571c6ea3310563373eea5c72d5aa9ecfac
maazalside <maazalside@gmail.com>
2024-03-06 00:31:37 +0300

    8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,7 +7,7 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    letter = {'q': sys.argv[1]}
+    letter = {'q': sys.argv[1] if len(sys.argv) > 1 else ""}
 
     req = requests.post('http://0.0.0.0:5000/search_user', data=letter)
     try:
